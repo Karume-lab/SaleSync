d9ddb05b9417d52159268c632d695c35860e0e98
Karume-lab <danielkarume@outlook.com>
2024-04-01 18:45:46 +0300

    merge pr

--- AdminDashboard.vb
+++ AdminDashboard.vb
@@ -444,6 +444,7 @@ Public Class AdminDashboard
             ' Close the database connection
             conn.Close()
         End Try
+    End Sub
     Private Sub TellerDashboard_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
         LoginForm.Show()
     End Sub
